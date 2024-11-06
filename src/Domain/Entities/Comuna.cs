bdb727757e629aae4e5457394fa28afd810fdee9
jhuenchunfinfast <jhuenchun@finfast.com>
2024-11-05 21:06:02 -0300

    feat(entities): modifica clase comuna

--- src/Domain/Entities/Comuna.cs
+++ src/Domain/Entities/Comuna.cs
@@ -1,9 +1,9 @@
 namespace WebApiAlertaMinsal.Domain.Entities;
 
-public class Comuna(string name, Region region)
+public class Comuna(string name)
 {
     public int Id { get; set; }
     public int IdRegion { get; set; }
     public string Name { get; set; } = name;
-    public virtual Region RegionNavigation { get; set; } = region;
+    public virtual Region Region { get; set; } = null!;
 }
