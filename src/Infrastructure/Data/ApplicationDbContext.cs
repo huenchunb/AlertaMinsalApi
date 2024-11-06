200c7e13cc52df010107f6983d27b713ffac9acd
jhuenchunfinfast <jhuenchun@finfast.com>
2024-11-05 23:05:39 -0300

    feat(infrastructure): implementa dbset en el contexto

--- src/Infrastructure/Data/ApplicationDbContext.cs
+++ src/Infrastructure/Data/ApplicationDbContext.cs
@@ -10,8 +10,14 @@ namespace WebApiAlertaMinsal.Infrastructure.Data;
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options), IApplicationDbContext
 {
+    public DbSet<ComplejidadEstablecimiento> Complejidades => Set<ComplejidadEstablecimiento>();
     public DbSet<Comuna> Comunas => Set<Comuna>();
+    public DbSet<Establecimiento> Establecimientos => Set<Establecimiento>();
+    public DbSet<NivelEstablecimiento> Niveles => Set<NivelEstablecimiento>();
     public DbSet<Region> Regiones => Set<Region>();
+    public DbSet<TipoAtencionEstablecimiento> TiposAtenciones => Set<TipoAtencionEstablecimiento>();
+    public DbSet<TipoEstablecimiento> TiposEstablecimientos => Set<TipoEstablecimiento>();
+    public DbSet<TipoUrgenciaEstablecimiento> TiposUrgencias => Set<TipoUrgenciaEstablecimiento>();
     public DbSet<TodoList> TodoLists => Set<TodoList>();
     public DbSet<TodoItem> TodoItems => Set<TodoItem>();
 
