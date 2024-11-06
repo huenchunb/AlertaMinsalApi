3986156c9a08b62a7f69dd53a83040ab7a02033d
jhuenchunfinfast <jhuenchun@finfast.com>
2024-11-06 01:13:40 -0300

    feat(getestablishmentbyidquery): se genera funcionalidad para obtener un establecimiento por id.

---
+++ src/Application/Establecimientos/Queries/GetEstablishmentById/GetEstablishmentById.cs
@@ -0,0 +1,23 @@
+using WebApiAlertaMinsal.Application.Common.Interfaces;
+using WebApiAlertaMinsal.Application.Common.Models;
+using WebApiAlertaMinsal.Application.Common.Security;
+using WebApiAlertaMinsal.Domain.Constants;
+
+namespace WebApiAlertaMinsal.Application.Establecimientos.Queries.GetEstablishmentById;
+
+[Authorize(Roles = Roles.Administrator)]
+public record GetEstablishmentByIdQuery(int Id) : IRequest<EstablishmentDto>;
+
+public class GetEstablishmentByIdQueryHandler(IApplicationDbContext context, IMapper mapper)
+    : IRequestHandler<GetEstablishmentByIdQuery, EstablishmentDto>
+{
+    public async Task<EstablishmentDto> Handle(GetEstablishmentByIdQuery request, CancellationToken cancellationToken)
+    {
+        var establishment = await context.Establecimientos.FindAsync(new object?[] { request.Id, cancellationToken },
+            cancellationToken: cancellationToken);
+
+        Guard.Against.NotFound(request.Id, establishment);
+
+        return mapper.Map<EstablishmentDto>(establishment);
+    }
+}
