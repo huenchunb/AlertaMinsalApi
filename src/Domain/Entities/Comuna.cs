19b80469c9eb1da1e31f0b3c88ec20c455262004
jhuenchunfinfast <jhuenchun@finfast.com>
2024-11-05 22:32:35 -0300

    refactor(entities): agrega idRegion a constructor comuna

--- src/Domain/Entities/Comuna.cs
+++ src/Domain/Entities/Comuna.cs
@@ -1,9 +1,9 @@
 namespace WebApiAlertaMinsal.Domain.Entities;
 
-public class Comuna(string name) : BaseAuditableEntity
+public class Comuna(string name, int idRegion) : BaseAuditableEntity
 {
-    public int IdRegion { get; set; }
     public string Name { get; set; } = name;
+    public int IdRegion { get; set; } = idRegion;
     public virtual Region Region { get; set; } = null!;
     public virtual ICollection<Establecimiento> Establecimientos { get; set; } = [];
 }
