d54642606c3a2c0e68e5374c6718b908e71b5a65
jhuenchunfinfast <jhuenchun@finfast.com>
2024-11-09 23:43:17 -0300

    fix(establecimientos): corrigen nombres de espacio
    Se corrigen nombres de espacios en establecimientos

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using AutoMapper.QueryableExtensions;
 global using Microsoft.EntityFrameworkCore;
 global using FluentValidation;
 global using MediatR;
+global using WebApiAlertaMinsal.Domain.Entities;
+global using WebApiAlertaMinsal.Application.Common.Interfaces;
